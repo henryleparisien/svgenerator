008004f201e08326f0759ba6b1b2d2febed8e3c2
hdeboisseguin@leparisien.fr <hdeboisseguin@leparisien.fr>
2020-01-06 15:11:46 +0100

    ADD salaires support

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,36 @@ var router = express.Router();
 var curl = require('curl');
 
 /* GET home page. */
+
+
+router.get('/salaires', function(req, res, next) {
+
+    var SPREADSHEETS_API_KEY = 'AIzaSyALk_Si5ynAuYeAy85WA7kv7t6nMN_7O88';
+    var ID = '10K_UVBFHUfpyO-W25rc0eUY_oPTaq0Hkl0dMPzSefoA';
+    var URL_SHEET = "https://sheets.googleapis.com/v4/spreadsheets/"+ID+"/values/A1:Z1000?key="+SPREADSHEETS_API_KEY;
+
+    curl.getJSON(URL_SHEET,{},function(err,resp, body){
+
+        var headers= body.values[0][0];
+        body.values.shift();
+        var donnees = body.values;
+
+        var fonction ="UTILISATEURS";
+        var donnees_out = [];
+        for (let i = 0; i < donnees.length; i++) {
+            const ligne = donnees[i];
+            if(ligne[1]==fonction){
+                donnees_out.push(ligne);
+            }
+        }
+        res.set("Content-Disposition", "attachment;filename="+fonction+".svg");
+        res.set("Content-Type", "image/svg+xml");
+        res.render('index_salaires', {headers: headers,fonction:fonction,donnees: donnees_out });
+    });
+    
+});
+
+
 router.get('/immo/ancien', function(req, res, next) {
 
   var SPREADSHEETS_API_KEY = 'AIzaSyALk_Si5ynAuYeAy85WA7kv7t6nMN_7O88';
@@ -41,32 +71,32 @@ router.get('/immo/ancien', function(req, res, next) {
     switch (dep) {
         case 'Paris_quartiers':
             var dep = 75;
-            var nom_dep="paris";
+            var nom_dep="Paris";
         break;
         case '93':
-            var nom_dep="seine-saint-denis";
+            var nom_dep="Seine-Saint-Denis";
         break;
         case '94':
-            var nom_dep="val-de-marne";
+            var nom_dep="Val-de-Marne";
         break;
         case '92':
-            var nom_dep="hauts-de-seine";
+            var nom_dep="Hauts-de-Seine";
         break;
         case '77':
-            var nom_dep="seine-et-marne";
+            var nom_dep="Seine-et-Marne";
         break;
         case '78':
-            var nom_dep="yvelines";
+            var nom_dep="Yvelines";
         break;
         case '91':
-            var nom_dep="essone";
+            var nom_dep="Essone";
         break;
         case '95':
-            var nom_dep="vald'oise";
+            var nom_dep="Val d'Oise";
         break;
         case 'Oise':
             var dep = 60
-            var nom_dep="oise";
+            var nom_dep="Oise";
         break;
     }
     console.log({ departement:dep, nom_dep:nom_dep,headers: headers,donnees: donnees });
